51deb984a4caf29bf6d320f9e71d8bd6edcc22c9
NaiLi <linnea.nabo@gmail.com>
2014-12-09 09:24:57 +0100

    only attend if logged in

--- attend.php
+++ attend.php
@@ -2,23 +2,10 @@
 
 	include "db_connect.php";
 
-	$eventid = $_POST['id'];
-	$name = $_POST['name'];
+	$eventid = $_POST['eventid'];
+	$userid = $_POST['userid'];
 
-	//TODO: check if user already exists
-
-	//First, create user (not checking if it exists)
-	$query_user = "INSERT INTO user (firstname, authority) VALUES ('$name', 2)";
-	
-	if (!mysql_query($query_user)) {
-		echo "Error: " . mysql_error();
-	}
-
-	//Get the userID for the new user
-	$userid = mysql_query("SELECT userID FROM user WHERE firstname = '$name'");
-	$userid = mysql_fetch_array($userid)['userID'];
-
-	//Then, connect the new user to the event
+	//insert into attending table
 	$query_attend = "INSERT INTO attending (eventID, userID)
 		VALUES ($eventid, $userid)";
 
