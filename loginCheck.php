b5067b83d72ec76edeebf2a4019592b384e66c08
emmwe459 <emmwe459@student.liu.se>
2014-12-09 10:14:32 +0100

    login check working

--- loginCheck.php
+++ loginCheck.php
@@ -9,17 +9,19 @@
 
 	$query = "SELECT * FROM user WHERE firstname = '$usr' AND pwd = '$pwd'";
 
-	$user = mysql_query($user);
+	$result = mysql_query($query);
 
 	// Login successful
-	if(mysql_num_rows($user) != 0) {
+	if(mysql_num_rows($result) != 0) {
+		$user = mysql_fetch_assoc($result);
 		$_SESSION['loggedin'] = true;
-		$_SESSION['user'] = "$usr";
-		$_SESSION['userid'] = "$usr[userID]";
-	}
-
-	include "db_disconnect.php";
-
-	header("Location: index.php?login_status_message=wrongCredentials");	
+		$_SESSION['user'] = $user['firstname'];
+		$_SESSION['userid'] = $user['userID'];
+		include "db_disconnect.php";
+		header("Location: index.php");
+	} else {
+		include "db_disconnect.php";
+		header("Location: index.php?login_status_message=wrongCredentials");
+	}	
 
 ?>
