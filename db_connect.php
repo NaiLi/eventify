9a0fb1311f587cbd6c55320d441b2eeba18f90e5
NaiLi <linnea.nabo@gmail.com>
2014-12-09 11:17:05 +0100

    test

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php
-	mysql_connect("localhost:8889", "root", "root")
+	mysql_connect("localhost:8889", "root", "")
 	//mysql_connect("localhost", "root", "")
 		or die("Could not connect");
 	mysql_select_db("eventify")
